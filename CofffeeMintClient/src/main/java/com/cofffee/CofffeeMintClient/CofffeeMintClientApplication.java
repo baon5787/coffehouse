a5748ea399d3b65414437133bb4d00558d6f0ab7
baon5787 <79984031+baon5787@users.noreply.github.com>
2021-04-26 20:13:24 +0700

    +intialize.Source

---
+++ CofffeeMintClient/src/main/java/com/cofffee/CofffeeMintClient/CofffeeMintClientApplication.java
@@ -0,0 +1,13 @@
+package com.cofffee.CofffeeMintClient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CofffeeMintClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CofffeeMintClientApplication.class, args);
+	}
+
+}
---
+++ CofffeeMintClient/src/test/java/com/cofffee/CofffeeMintClient/CofffeeMintClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cofffee.CofffeeMintClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CofffeeMintClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
